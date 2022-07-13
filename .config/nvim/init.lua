c9494607ff1182d635c1a7e656504216746ec1e0
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-07-12 20:11:37 -0400

    Status line colors with mode switches. Also global status line :smile:

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,35 +33,50 @@ endfunction
 com! DiffSaved call s:DiffWithSaved()
 ]])
 
+-- vim.cmd([[
+-- hi User1 guifg=black guibg=gray    ctermfg=black ctermbg=gray
+-- hi User2 guifg=black guibg=blue    ctermfg=black ctermbg=blue
+-- hi User3 guifg=black guibg=magenta ctermfg=black ctermbg=magenta
+-- hi User4 guifg=black guibg=yellow  ctermfg=black ctermbg=yellow
+-- hi User5 guifg=black guibg=cyan    ctermfg=black ctermbg=cyan
+-- hi User6 guifg=white guibg=black   ctermfg=white  ctermbg=black
+-- ]])
 vim.cmd([[
-hi User1 guifg=black guibg=gray    ctermfg=black ctermbg=gray
-hi User2 guifg=black guibg=blue    ctermfg=black ctermbg=blue
-hi User3 guifg=black guibg=magenta ctermfg=black ctermbg=magenta
-hi User4 guifg=black guibg=yellow  ctermfg=black ctermbg=yellow
-hi User5 guifg=black guibg=cyan    ctermfg=black ctermbg=cyan
-hi User6 guifg=white guibg=black   ctermfg=white  ctermbg=black
+hi! NormalColor		guifg=black guibg=gray		ctermfg=black	ctermbg=gray
+hi! InsertColor		guifg=black guibg=#fabd2f	ctermfg=black	ctermbg=214
+hi! ReplaceColor	guifg=black guibg=#b8bb26	ctermfg=black	ctermbg=142
+hi! VisualColor		guifg=black guibg=#d3869b	ctermfg=black	ctermbg=175
+hi! User6	guifg=white	guibg=black	ctermfg=white	ctermbg=black
 ]])
 
-vim.cmd("au InsertLeave * hi User1 guibg=grey ctermbg=grey")
-vim.cmd("au InsertEnter * hi User1 guibg=#98971a ctermbg=green")
+-- Highlight things you yank
+vim.api.nvim_create_autocmd("TextYankPost",
+	{ pattern = "*", callback = function() vim.highlight.on_yank({ timeout = 70 }) end })
 
--- " always displays statusline
 vim.cmd([[
 let g:currentmode={
-	\ 'n'  : 'NORMAL ',
-	\ 'v'  : 'VISUAL ',
-	\ 'V'  : 'V·Line ',
-	\ '' : 'V·Block ',
-	\ 'i'  : 'INSERT ',
-	\ 'R'  : 'R ',
-	\ 'Rv' : 'V·Replace ',
-	\ 'c'  : 'Command ',
-	\ 's'  : 'SELECT ',
-	\ 't'  : 'TESTING ',
+	\ 'n'  : 'NORMAL',
+	\ 'v'  : 'VISUAL',
+	\ 'V'  : 'V·Line',
+	\ '' : 'V·Block',
+	\ 'i'  : 'INSERT',
+	\ 'R'  : 'R',
+	\ 'Rv' : 'V·Replace',
+	\ 'c'  : 'COMMAND',
+	\ 's'  : 'SELECT',
+	\ 't'  : 'FINDER',
 	\}
 
 set statusline=
-set statusline+=%1*\ %1*%{toupper(g:currentmode[mode()])}
+set statusline+=%#NormalColor#%{(g:currentmode[mode()]=='NORMAL')?'\ \ NORMAL\ ':''}
+set statusline+=%#InsertColor#%{(g:currentmode[mode()]=='INSERT')?'\ \ INSERT\ ':''}
+set statusline+=%#ReplaceColor#%{(g:currentmode[mode()]=='R')?'\ \ REPLACE\ ':''}
+set statusline+=%#ReplaceColor#%{(g:currentmode[mode()]=='V·Replace')?'\ \ V-REPLACE\ ':''}
+set statusline+=%#VisualColor#%{(g:currentmode[mode()]=='VISUAL')?'\ \ VISUAL\ ':''}
+set statusline+=%#VisualColor#%{(g:currentmode[mode()]=='V·Line')?'\ \ V-LINE\ ':''}
+set statusline+=%#VisualColor#%{(g:currentmode[mode()]=='V·Block')?'\ \ V-BLOCK\ ':''}
+set statusline+=%#NormalColor#%{(g:currentmode[mode()]=='COMMAND')?'\ \ COMMAND\ ':''}
+set statusline+=%#NormalColor#%{(g:currentmode[mode()]=='FINDER')?'\ \ FINDER\ ':''}
 set statusline+=%6*\ %f
 set statusline+=\ %6*\ %{&modified?'[+]':''}
 set statusline+=\ %=
@@ -70,3 +85,4 @@ set statusline+=\ %1*\ %v:%l\/%L
 set statusline+=\ %6*\ [%p%%]
 set statusline+=\ "
 ]])
+vim.opt.laststatus=3
