3aa1c8fe9363d766d3e55cefb5b566ea4bd7b67d
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-07-16 08:43:17 -0400

    Luasnip stuff

--- .config/nvim/lua/pluginconf/luasnip.lua
+++ .config/nvim/lua/pluginconf/luasnip.lua
@@ -8,6 +8,7 @@ local f = luasnip.function_node
 local c = luasnip.choice_node
 local d = luasnip.dynamic_node
 local r = luasnip.restore_node
+local sn = luasnip.snippet_node
 local rep = require('luasnip.extras').rep
 local fmt = require('luasnip.extras.fmt').fmt
 
@@ -45,13 +46,28 @@ luasnip.add_snippets("c", {
 
 luasnip.add_snippets("lua", {
 	-- Example of a main function snippet in C. Choice node for cmdline arguments
-	s("require", fmt([[local {} = require("{}")]], { f(function (import_name)
+	s("require", fmt([[local {} = require("{}")]], { f(function(import_name)
 		-- Gives each part. Like `str.split()` in python
 		local parts = vim.split(import_name[1][1], ".", true)
 		return parts[#parts] or ""
-	end, {1}), i(1, "modname") })),
+	end, { 1 }), i(1, "modname") })),
 
 
+})
+
+luasnip.add_snippets("all", {
+	s("rc", fmt([[
+		const {} = ({}) => {{
+			{}
+		}}
+
+		export default {};]],
+		{
+			d(1, function() return sn(nil, i(1, vim.fn.expand('%:t:r'))) end),
+			i(2),
+			i(3),
+			f(function(argnodes) return argnodes[1][1] end, { 1 }),
+		})),
 })
 
 require("luasnip.loaders.from_snipmate").load()
