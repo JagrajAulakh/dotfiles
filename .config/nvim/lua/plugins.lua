f339c5adad0e91869cae1bf43d3c6c5c28af66bd
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-07-24 12:04:17 -0400

    Cleaning up plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -10,6 +10,7 @@ Plug 'kevinhwang91/rnvimr'
 
 -- Comment blocks of code
 Plug 'numToStr/Comment.nvim'
+Plug 'JoosepAlviste/nvim-ts-context-commentstring'
 
 Plug 'tpope/vim-dispatch'
 
@@ -17,16 +18,17 @@ Plug 'tpope/vim-dispatch'
 Plug 'tpope/vim-fugitive'
 Plug 'mhinz/vim-signify' -- Show's git additions/modifications/deletions in number column
 
-Plug 'jremmen/vim-ripgrep'
-
--- NVIM LSP STUFF
-Plug 'nvim-lua/plenary.nvim'
+-- Telescope
+Plug 'nvim-lua/plenary.nvim' -- Dependency for telescope
 Plug 'nvim-telescope/telescope.nvim'
 
-Plug 'neovim/nvim-lspconfig' -- NVIM LSP
+-- NVIM LSP STUFF
+Plug 'neovim/nvim-lspconfig'
 Plug 'jose-elias-alvarez/null-ls.nvim'
 Plug 'nvim-treesitter/nvim-treesitter'
-Plug 'JoosepAlviste/nvim-ts-context-commentstring'
+
+-- PRETTIER
+Plug 'MunifTanjim/prettier.nvim' -- Uses null-ls plugin
 
 -- LSP COMPLETION
 Plug 'hrsh7th/nvim-cmp'
@@ -36,6 +38,7 @@ Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-nvim-lua'
 Plug 'hrsh7th/cmp-cmdline'
 
+-- Snippets
 Plug 'saadparwaiz1/cmp_luasnip'
 Plug 'L3MON4D3/LuaSnip'
 
@@ -47,18 +50,9 @@ Plug 'morhetz/gruvbox'
 Plug 'dracula/vim'
 Plug 'norcalli/nvim-colorizer.lua' -- Fast color highlighter written in lua
 
--- For automatically reloading buffer on file change
-Plug 'djoshea/vim-autoread'
-
 -- SYNTAX
 Plug 'tikhomirov/vim-glsl'       -- GLSL
 Plug 'mboughaba/i3config.vim'    -- i3wm
-Plug 'yuezk/vim-js'              -- JavaScript
-Plug 'maxmellon/vim-jsx-pretty'  -- JSX (JavaScript XML)
-
--- PRETTIER
--- *Uses null-ls plugin
-Plug 'MunifTanjim/prettier.nvim'
 
 -- Markdown stuff
 Plug 'ixru/nvim-markdown'
@@ -68,7 +62,6 @@ Plug 'nvim-lualine/lualine.nvim'
 Plug 'kyazdani42/nvim-web-devicons'
 
 vim.call('plug#end')
-
 
 
 -- PLUGIN CONFIGURATIONS
